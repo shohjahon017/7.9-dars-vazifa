5998a48afb63a618720ccef67a2cfee761ed7c8f
shohjhahon017 <shohjahonsoilov1@gmail.com>
2026-01-13 22:40:26 +0500

    C:/Program Files/Git/

--- src/data/teachers.jsx
+++ src/data/teachers.jsx
@@ -8,24 +8,24 @@ const teachers = [
     image: "/teachers/ielts.jpg",
     desc: "IELTS va Ingliz tili bo‘yicha 300+ o‘quvchi tayyorlagan professional mentor.",
   },
-  {
-    slug: "behruz-math",
-    name: "Behruz",
-    subject: "Matematika",
-    experience: "7 yil",
-    score: "DTM ekspert",
-    image: "/teachers/math.jpg",
-    desc: "Abituriyentlar va maktab o‘quvchilari bilan katta tajriba.",
-  },
-  {
-    slug: "arab-tili-mentor",
-    name: "Ahmad",
-    subject: "Arab tili",
-    experience: "6 yil",
-    score: "B2 sertifikat",
-    image: "/teachers/arab.jpg",
-    desc: "Arab tili grammatikasi va muloqot bo‘yicha mutaxassis.",
-  },
+  // {
+  //   slug: "behruz-math",
+  //   name: "Behruz",
+  //   subject: "Matematika",
+  //   experience: "7 yil",
+  //   score: "DTM ekspert",
+  //   image: "/teachers/math.jpg",
+  //   desc: "Abituriyentlar va maktab o‘quvchilari bilan katta tajriba.",
+  // },
+  // {
+  //   slug: "arab-tili-mentor",
+  //   name: "Ahmad",
+  //   subject: "Arab tili",
+  //   experience: "6 yil",
+  //   score: "B2 sertifikat",
+  //   image: "/teachers/arab.jpg",
+  //   desc: "Arab tili grammatikasi va muloqot bo‘yicha mutaxassis.",
+  // },
 ];
 
 export default teachers;
