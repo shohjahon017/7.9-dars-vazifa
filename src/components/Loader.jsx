d464dee1752b378c9ada05bf96bfa93f7b3eaff3
shohjhahon017 <shohjahonsoilov1@gmail.com>
2026-01-14 14:08:10 +0500

    logo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,16 +2,16 @@ import { motion } from "framer-motion";
 
 export default function Loader() {
   return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-[#05070f]">
+    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-[#05070f]">
+      <img src="/universe.png" alt="" />{" "}
       <motion.div
         animate={{ rotate: 360 }}
         transition={{ repeat: Infinity, duration: 1.2, ease: "linear" }}
-        className="w-20 h-20 rounded-full border-4 border-blue-500/30 border-t-blue-500"
+        className="w-14 h-14 rounded-full border-2 border-blue-500/30 border-t-blue-500"
       />
-
-      <p className="absolute bottom-16 text-white/60 text-sm tracking-wide">
-        Universe loading...
-      </p>
+      {/* <p className="absolute bottom-16 text-white/60 text-sm tracking-wide">
+        Loading...
+      </p> */}
     </div>
   );
 }
